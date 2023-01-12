59f25231f4f364e83f542852f698b0215a65ee1e
Sandratrahasina <rakoto.sandratrahasina@gmail.com>
2023-01-12 10:16:18 +0300

    first commit

---
+++ src/main/java/me/cnam/tononkira/TononkiraApplication.java
@@ -0,0 +1,13 @@
+package me.cnam.tononkira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TononkiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TononkiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/cnam/tononkira/TononkiraApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cnam.tononkira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TononkiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
